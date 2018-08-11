a41ae8feacde5f4e032d32b83e6822c57e3ce529
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-12 03:29:53 +0900

    edit pages/mypage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
   def mypage
-    @comments = Comment.all.order('updated_at DESC')
-    @user = User.find(2)
-    # posts = @user.posts.where(achieved: true)
-    # @drops = 
-
     render json: {
       "user": [
         {
@@ -15,7 +10,7 @@ class PagesController < ApplicationController
           "name" => "bb",
           "id" => 2
         }
-      ],
+      ]
     }
   end
   def users

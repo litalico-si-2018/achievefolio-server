40e4da08140789a3386fc564350d866d6401b4ab
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-12 03:34:32 +0900

    edit pages/mypage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,26 @@
 class PagesController < ApplicationController
   def mypage
-    @comments = Comment.all.order('updated_at DESC')
-    @user = User.find(2)
+    @user = User.find(1)
     # posts = @user.posts.where(achieved: true)
     # @drops = 
 
-    render
-        render json: {
-      "user": [
-        {
-          "name" => "aa",
-          "id" => 1
-        },
-        {
-          "name" => "bb",
-          "id" => 2
-        }
-      ],
-      "comment":
-        @comments.map {|f|
-          [id: f.id,c: f.body]
-        }
-    }
+    # render
+    #     render json: {
+    #   "user": [
+    #     {
+    #       "name" => "aa",
+    #       "id" => 1
+    #     },
+    #     {
+    #       "name" => "bb",
+    #       "id" => 2
+    #     }
+    #   ],
+    #   "comment":
+    #     @comments.map {|f|
+    #       [id: f.id,c: f.body]
+    #     }
+    # }
   end
   def users
     @comments = Comment.all.order('updated_at DESC')

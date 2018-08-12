9e756f4dea245c0f71aaf9560b65255b90b0f9af
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-12 10:15:11 +0900

    sdfasiouyfsigudsfjxcvhxkjghkkl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::API
   before_action :get_user
   
   def get_user
-  	if request.headers["achievefolio-user-id"] then
-      @current_user = User.find(request.headers["achievefolio-user-id"])
-    else
+  	# if request.headers["achievefolio-user-id"] then
+   #    @current_user = User.find(request.headers["achievefolio-user-id"])
+   #  else
       @current_user = User.find(1)
-    end
+    # end
   end
 end

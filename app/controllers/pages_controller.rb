22c584b420119551f577a9809b514b3124b850f4
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-12 10:09:47 +0900

    sdfasiouyfsigudsfjxcvhxkjghkkl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
   def mypage
     @user = User.find(1)
     @achi = Achievement.all
-    @posts = Post.all
+    @posts = @user.posts
+
+    =@user.posts[0].year
+    tmp[1]=@user.posts[0].month
+    tmp[2]=@user.posts[0].date
+
   end
   def users
     @comments = Comment.all.order('updated_at DESC')

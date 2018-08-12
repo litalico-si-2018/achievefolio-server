d5524c1e34a9bf0d9e4b84c4c5f35a77b06817a4
kthatoto <kthatoto@gmail.com>
2018-08-12 12:29:39 +0900

    Edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,33 +9,3 @@ User.create(
   icon: "yamamoto.jpg",
   role: "admin"
 )
-
-for i in 1..user_num do
-  User.create!(name: "ユーザー#{i}", role: i%2)
-  for j in 1..post_num do
-    Post.create!(
-      user_id: i,
-      body: "aaa",
-      approved_id: 2,
-      approved: false,
-      year: 2018,
-      month: 7,
-      date: 5
-    )
-    for k in 1..comment_num do
-      Comment.create!(
-        body: "投稿",
-        user_id: i,
-        post_id: j,
-        status: 0
-      )
-    end
-  end
-end
-
-for i in 1..achievement_num do
-  Achievement.create!(
-      value: 1,
-      status: 0
-  	)
-end

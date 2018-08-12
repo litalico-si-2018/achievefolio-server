c76613deee30efea2b5a80a326959d65c3bcc3f8
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-12 13:11:22 +0900

    post/create(body)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,28 +6,37 @@ class PostsController < ApplicationController
     @post.date = Time.now.day
     @post.approved_id = 2
     @post.approved = false
+    @post.body = params[:body]
 
     @post.save
 
-    achieves = []
-    Achievement.where(type: 0).each do |a|
-      next if UserAchievement.find_by(user_id: @current_user.id, achievement_id: a.id) # nil or Achievement
-      ua = UserAchievement.create(user_id: @current_user.id, achievement_id: a.id)
-      achieves << { id: ua.id }
+
+    if (@post.body.length > 10) then
+      ach = Achievement.find(1)
+      ach.achieved = true
+      ach.update(ach_params)
     end
-    if achieves.length > 0
-      render json: @post.attributes.merge({
-        achieves: achieves
-      })
-    else
-      render json: @post.attributes
-    end
+
+    # achieves = []
+    # Achievement.where(type: 0).each do |a|
+    #   next if UserAchievement.find_by(user_id: @current_user.id, achievement_id: a.id) # nil or Achievement
+    #   ua = UserAchievement.create(user_id: @current_user.id, achievement_id: a.id)
+    #   achieves << { id: ua.id }
+    # end
+    # if achieves.length > 0
+      render json: @post
+    # else
+    #   render json: @post.attributes
+    # end
+
+    
+
 
     # render json: @post
   end
 
   def approved
-  	@post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @post.approved = true
     @post.update(post_params)
 
@@ -41,6 +50,9 @@ class PostsController < ApplicationController
   private
     def post_params
       params.fetch(:post, {}).permit(:approved)
+    end
+    def ach_params
+      params.fetch(:achievement, {}).permit(:achieved)
     end
 
 
